86a7ac2f1ef74c555084013ac97dfe82eb249722
Zeyln <ragnar.lintoft@gmail.com>
2026-03-09 13:07:34 +0100

    Started making my first component, working in nvim at same time.

---
+++ src/components/gmananim.tsx
@@ -0,0 +1,7 @@
+export default function Gman() {
+    return (
+        <div className="player">
+            <img className="gifs" src="https://media.tenor.com/rzzFtSkU92wAAAAM/gman-half-life.gif" alt="the gman is schmoovins" />
+        </div>
+    );
+}
