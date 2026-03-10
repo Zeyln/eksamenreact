129d27567f6d0e09505c7059206f605e9ef022a6
Zeyln <ragnar.lintoft@gmail.com>
2026-03-10 09:30:19 +0100

    Made connection to bitcoin websocket. The component messaging the websocket is going to have some more parameters added, removed the original test element

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import PullStocks from "./components/massivewebsocket.tsx"
-import Gman from "./components/gmananim.tsx"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <PullStocks />
-      <Gman />
     </>
   )
 }
--- src/components/gmananim.tsx
+++
@@ -1,7 +0,0 @@
-export default function Gman() {
-    return (
-        <div className="player">
-            <img className="gifs" src="https://media.tenor.com/rzzFtSkU92wAAAAM/gman-half-life.gif" alt="the gman is schmoovins" />
-        </div>
-    );
-}
